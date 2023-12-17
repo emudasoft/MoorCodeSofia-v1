35b8e2578d901a438778201f98829c84459d3536
emudasoft <yildiz34murat@gmail.com>
2023-12-17 01:44:44 +0000

    Valid date format  must be dd/MM/yyyy control added

--- MoorCodeSofia.Application/UserTasks/Commands/CreateUserTaskCommandValidator.cs
+++ MoorCodeSofia.Application/UserTasks/Commands/CreateUserTaskCommandValidator.cs
@@ -7,9 +7,9 @@ namespace MoorCodeSofia.Application.UserTasks.Commands
         public CreateUserTaskCommandValidator()
         {
             RuleFor(x => x.EndDate).GreaterThan(x => x.StartDate).NotNull().WithMessage("EndDate cannot be null or less then StartDate");
-            RuleFor(x => x.EndDate).Must(BeValidDate).WithMessage("EndDate must be a validate date");
+            RuleFor(x => x.EndDate).Must(BeValidDate).WithMessage("EndDate must be a valid date. Valid date format  is dd/MM/yyyy ");
             RuleFor(x => x.StartDate).LessThan(x => x.EndDate).NotNull().WithMessage("StartDate cannot be null or less then EndDate");
-            RuleFor(x => x.StartDate).Must(BeValidDate).WithMessage("StartDate must be a validate date");
+            RuleFor(x => x.StartDate).Must(BeValidDate).WithMessage("StartDate must be a valid date. Valid date format  is dd/MM/yyyy");
 
         }
         private bool BeValidDate(DateTime date) { return !date.Equals(default); }
