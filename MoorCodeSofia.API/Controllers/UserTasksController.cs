f75773d95c1115cd7bc054dfa8293f937d8bd4ff
emudasoft <yildiz34murat@gmail.com>
2023-12-17 01:24:18 +0000

    Validator control added for all request

--- MoorCodeSofia.API/Controllers/UserTasksController.cs
+++ MoorCodeSofia.API/Controllers/UserTasksController.cs
@@ -1,14 +1,20 @@
-﻿using MediatR;
+﻿using FluentValidation.TestHelper;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using MoorCodeSofia.API.Abstractions;
 using MoorCodeSofia.Application.UserTasks.Commands;
 using MoorCodeSofia.Application.UserTasks.Queries;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace MoorCodeSofia.API.Controllers
 {
     [Route("api/userTasks")]
     public class UserTasksController : ApiController
     {
+        private readonly CreateUserTaskCommandValidator _createValidator = new();
+        private readonly UpdateUserTaskCommandValidator _updateValidator= new();
+        private readonly DeleteUserTaskCommandValidator _deleteValidator = new();
+        private readonly GetUserTaskCommandValidator  _getTaskByIdValidator= new();
         public UserTasksController(ISender sender) : base(sender)
         {
         }
@@ -17,33 +23,36 @@ namespace MoorCodeSofia.API.Controllers
         public async Task<IActionResult> CreateUserTask(CreateUserTaskCommand command,
             CancellationToken cancellationToken)
         {
-            var createCommand = new CreateUserTaskCommand(
 
-                command.User,
-                command.Description,
-                command.StartDate,
-                command.EndDate,
-                command.Subject);
+            var requestCheckResult = await Task.Run(() => _createValidator.TestValidate(command));
+            if (!requestCheckResult.IsValid) return Ok(requestCheckResult.Errors);
+             
+                var createCommand = new CreateUserTaskCommand(
 
-            var response = await Sender.Send(createCommand, cancellationToken);
-            return Ok(response);
+                    command.User,
+                    command.Description,
+                    command.StartDate,
+                    command.EndDate,
+                    command.Subject);
+
+                var response = await Sender.Send(createCommand, cancellationToken);
+                return Ok(response);
 
         }
         [HttpGet("getAllTasks")]
         public async Task<IActionResult> GetAllUserTasks(CancellationToken cancellationToken)
         {
+            
             var query = new GetAllUserTasksQuery(0, 10);
             var response = await Sender.Send(query, cancellationToken);
             return response.IsSuccess ? Ok(response) : NotFound(response.Error);
+
         }
         [HttpGet("getTasksById")]
         public async Task<IActionResult> GeTasksById(GetUserTaskCommand command, CancellationToken cancellationToken)
         {
-            if (command.Id == Guid.Empty)
-            {
-                return Ok();
-            }
-
+            var requestCheckResult = await Task.Run(() => _getTaskByIdValidator.TestValidate(command));
+            if (!requestCheckResult.IsValid) return Ok(requestCheckResult.Errors);
             var getCommand = new GetUserTaskCommand(
                command.Id
               );
@@ -56,6 +65,8 @@ namespace MoorCodeSofia.API.Controllers
         public async Task<IActionResult> UpdateUserTask(UpdateUserTaskCommand command,
             CancellationToken cancellationToken)
         {
+            var requestCheckResult = await Task.Run(() => _updateValidator.TestValidate(command));
+            if (!requestCheckResult.IsValid) return Ok(requestCheckResult.Errors);
             var createCommand = new UpdateUserTaskCommand(
                 command.Id,
                 command.User,
@@ -73,6 +84,8 @@ namespace MoorCodeSofia.API.Controllers
         public async Task<IActionResult> DeleteUserTask(DeleteUserTaskCommand command,
             CancellationToken cancellationToken)
         {
+            var requestCheckResult = await Task.Run(() => _deleteValidator.TestValidate(command));
+            if (!requestCheckResult.IsValid) return Ok(requestCheckResult.Errors);
             var createCommand = new DeleteUserTaskCommand(
                 command.id
                );
